4aadbfdbaeb92e5574534f336a2f0a9322319ac5
홍승찬 <92988513+Weekndd@users.noreply.github.com>
2024-12-19 17:15:47 +0900

    [Platinum V] Title: 도로포장, Time: 760 ms, Memory: 65808 KB -BaekjoonHub

---
+++ 백준/Platinum/1162. 도로포장/도로포장.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M, K;
+	static class Node {
+		int S,E,cnt;
+		long W;
+		public Node(int S, int E, long W, int cnt) {
+			this.S = S;
+			this.E = E;
+			this.W = W;
+			this.cnt = cnt;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		long[][] dp = new long[N+1][K+1];
+		List<Node>[] way = new ArrayList[N+1];
+		for(int i=1; i<=N; i++) {
+			way[i] = new ArrayList<>();
+			Arrays.fill(dp[i],Long.MAX_VALUE);
+		}
+		
+		boolean[][] check = new boolean[N+1][K+1];
+		PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingLong(o1-> o1.W));
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			way[s].add(new Node(s,e,w,0));
+			way[e].add(new Node(e,s,w,0));
+		}
+		pq.add(new Node(1,1,0,0));
+		dp[1][0] = 0;
+		
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			if(check[now.E][now.cnt]) continue;
+			check[now.E][now.cnt] = true;
+			for(Node next : way[now.E]) {
+				long nextCost = now.W+next.W;
+				if(nextCost < dp[next.E][now.cnt]) {
+					dp[next.E][now.cnt] = nextCost;
+					pq.offer(new Node(next.S, next.E, nextCost, now.cnt));
+				}
+				if(now.cnt<K && now.W<dp[next.E][now.cnt+1]) {
+					dp[next.E][now.cnt+1] = now.W;
+					pq.offer(new Node(next.S, next.E, now.W, now.cnt+1));
+				}
+			}
+		}//end while
+		
+		long min = Long.MAX_VALUE;
+		for(long cost : dp[N]) {
+			min = Math.min(min,cost);
+		}
+		System.out.println(min);
+	}//end main
+}
