c3c7ae10ccdc30d84256c4b49c9165db31dcac11
홍승찬 <92988513+Weekndd@users.noreply.github.com>
2024-12-26 14:50:24 +0900

    [Silver I] Title: 보석 상자, Time: 404 ms, Memory: 39384 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N,M,jams[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        jams = new int[M];
+        for(int i=0; i<M; i++) {
+        	jams[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println(parametricSearch(N));
+    }//end main
+    
+    static int parametricSearch(int target) {
+    	int max = Arrays.stream(jams).max().getAsInt();
+    	int left = 1;
+    	int right = max;
+    	
+    	while(left<=right) {
+    		int mid = left+(right-left)/2;
+    		int sum = 0; //나눠줄 인원
+    		for(int i=0; i<M; i++) {
+    			sum += jams[i]/mid;
+    			if(jams[i]%mid!=0) sum+=1;
+    		}
+    		if(sum>target) {
+    			left = mid + 1;
+    		}
+    		else {
+    			right = mid - 1;
+    		}
+    	}
+    	return left;
+    }
+}
