3f43926a950190d4c67dcc1399997cad13b325d1
홍승찬 <92988513+Weekndd@users.noreply.github.com>
2025-01-30 13:24:10 +0900

    [Gold V] Title: 사과나무, Time: 256 ms, Memory: 21016 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.java
+++ 백준/Gold/20002. 사과나무/사과나무.java
@@ -20,18 +20,15 @@ class Main {
         
         int max = Integer.MIN_VALUE;
         for(int i=1; i<=N; i++) {
-        	for(int j=1; j<=N; j++) {
-        		for(int a=0; a<i; a++) {
-        			for(int b=0; b<j; b++) {
-        				if(i-a != j-b) continue;
-        				int sum = arr[i][j]
-        						- arr[a][j]
-        						- arr[i][b]
-        						+ arr[a][b];
-        				max = Math.max(sum,max);
-        			}
-        		}
-        	}
+        	for(int a=0; a<=N-i; a++) {
+    			for(int b=0; b<=N-i; b++) {
+    				int sum = arr[a+i][b+i]
+    						- arr[a][b+i]
+    						- arr[a+i][b]
+    						+ arr[a][b];
+    				max = Math.max(sum,max);
+    			}
+    		}
         }
         System.out.println(max);
     }//end main
