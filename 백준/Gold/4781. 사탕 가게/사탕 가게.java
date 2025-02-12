438e7c922b37e76ffc54a7911567145d51e28179
홍승찬 <92988513+Weekndd@users.noreply.github.com>
2025-02-12 22:37:47 +0900

    [Gold IV] Title: 사탕 가게, Time: 1224 ms, Memory: 37920 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -16,27 +16,25 @@ class Main {
     	while(true) {
     		StringTokenizer st = new StringTokenizer(br.readLine());
     		int N = Integer.parseInt(st.nextToken()); //사탕 종류
-    		double K = Double.parseDouble(st.nextToken()); //가진 돈
+    		int K = Math.round(100*Float.parseFloat(st.nextToken())); //가진 돈
     		if(N==0 && K==0.00) break;
     		Candy[] candies = new Candy[N+1];
     		for(int i=1; i<=N; i++) {
     			st = new StringTokenizer(br.readLine());
     			int kcal = Integer.parseInt(st.nextToken()); //사탕 종류
-    			double price = Double.parseDouble(st.nextToken()); //가진 돈
-    			candies[i] = new Candy(kcal, (int) Math.round(price * 100)); // 정확한 변환
+    			int price = Math.round(100*Float.parseFloat(st.nextToken())); //가진 돈
+    			candies[i] = new Candy(kcal, price); // 정확한 변환
     		}
     		
-    		int[]dp = new int[(int)(K*100)+1];
-    		dp[0] = 0;
-    		
+    		int[]dp = new int[K+1];
     		for(int i=1; i<=N; i++) {
     			int kcal = candies[i].kcal;
     			int price = candies[i].price;
-    			for(int j=price; j<=(int)(K*100); j++) {
+    			for(int j=price; j<=K; j++) {
     				dp[j] = Math.max(dp[j],dp[j-price]+kcal);
     			}
     		}
-    		sb.append(dp[(int)(K*100)]+"\n");
+    		sb.append(dp[K]+"\n");
     	}
     	System.out.println(sb);
     	
