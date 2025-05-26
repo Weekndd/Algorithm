e909db0c33954be9cbb2bf81cfa6daadcf9f730a
SeungChan Hong <92988513+Weekndd@users.noreply.github.com>
2025-05-26 10:50:14 +0900

    [Gold V] Title: 행복 유치원, Time: 540 ms, Memory: 65156 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int[] temp = new int[N-1];
+        for(int i=1; i<N; i++) {
+        	temp[i-1] = arr[i]-arr[i-1];
+        }
+        Arrays.sort(temp);
+        int ans = 0;
+        for(int i=0; i<(N-1)-(K-1); i++) {
+        	ans+=temp[i];
+        }
+        System.out.println(ans);
+    }//end main
+}
