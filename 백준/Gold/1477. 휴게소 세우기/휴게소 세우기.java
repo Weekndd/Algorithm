97b33a85de4855cab953b145a7a78421563db773
SeungChan Hong <92988513+Weekndd@users.noreply.github.com>
2025-07-11 21:32:07 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 68 ms, Memory: 11636 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+2];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        arr[N+1] = L;
+        Arrays.sort(arr);
+        int left = 1;
+        int right = L-1;
+        while(left<=right) {
+            int mid = left+(right-left)/2;
+            int sum = 0;
+            for(int i=1; i<=N+1; i++) {
+                sum += (arr[i]-arr[i-1]-1)/mid;
+            }
+            if(sum>M) {
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+        System.out.println(left);
+    }//end main
+}
