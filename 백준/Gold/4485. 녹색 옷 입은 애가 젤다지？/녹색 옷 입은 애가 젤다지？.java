b828f235641d05173cc881f36fb032ba2aa2539a
SeungChan Hong <92988513+Weekndd@users.noreply.github.com>
2026-04-14 23:31:15 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 148 ms, Memory: 17512 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,69 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static public class Node implements Comparable<Node>{
-		int row, col, wei;
-		public Node(int row, int col, int wei) {
-			super();
-			this.row = row;
-			this.col = col;
-			this.wei = wei;
+	static int[] dirR = new int[] {0,0,1,-1};
+	static int[] dirC = new int[] {1,-1,0,0};
+	public static class Node implements Comparable<Node>{
+		int r,c,w;
+		Node(int r, int c, int w) {
+			this.r = r;
+			this.c = c;
+			this.w = w;
 		}
 		@Override
-		public int compareTo(Node o) {
-			return this.wei - o.wei;
+		public int compareTo(Node n) {
+			return this.w - n.w;
 		}
 	}
-	static int[] dirR = new int[] {0,0,-1,1};
-	static int[] dirC = new int[] {-1,1,0,0};
-	
-	public static void main(String[] args) throws Exception { //녹색 옷 입은 애가 젤다지?
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		int t = 1;
+		int t = 0;
 		while(true) {
 			int N = Integer.parseInt(br.readLine());
 			if(N==0) break;
-			
 			int[][] map = new int[N][N];
 			for(int i=0; i<N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int j=0; j<N; j++) {
+				for(int j=0; j<N; j++ ) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
-			}//end input
-			
-			int[][] minCost = new int[N][N];
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					minCost[i][j] = Integer.MAX_VALUE;
-				}
-			}//초기값 세팅
-			
-			boolean[][] visited = new boolean[N][N];
+			}
 			PriorityQueue<Node> pq = new PriorityQueue<>();
+			int[][] cost  = new int[N][N];
+			for(int i=0; i<N; i++) Arrays.fill(cost[i], Integer.MAX_VALUE);
+			cost[0][0] = map[0][0];
 			pq.offer(new Node(0,0,map[0][0]));
-			minCost[0][0] = map[0][0];
 			
 			while(!pq.isEmpty()) {
 				Node now = pq.poll();
-				visited[now.row][now.col]=true;	
-				if(now.row==N-1 && now.col==N-1) break;
-				
-				for(int i=0; i<4; i++) {
-					int nr = now.row+dirR[i];
-					int nc = now.col+dirC[i];
+				if(now.r == N-1 && now.c == N-1) break;
+				for(int d=0; d<4; d++) {
+					int nr = now.r + dirR[d];
+					int nc = now.c + dirC[d];
 					if(nr<0 || nr>=N || nc<0 || nc>=N) continue;
-					if(visited[nr][nc]) continue;
-					if(minCost[nr][nc] > now.wei + map[nr][nc]) {
-						minCost[nr][nc] = now.wei + map[nr][nc];
-						pq.offer(new Node(nr,nc,minCost[nr][nc]));
-					}
+					if(cost[nr][nc] <= now.w + map[nr][nc]) continue;
+					pq.offer(new Node(nr, nc, now.w + map[nr][nc]));
+					cost[nr][nc] = now.w + map[nr][nc];;
 				}
 			}
-			sb.append("Problem "+(t++)+": "+minCost[N-1][N-1]+"\n");
-		}//end testcase
+			sb.append("Problem " + ++t+": "+cost[N-1][N-1]+"\n");
+		}
 		System.out.println(sb);
-	}//end main
+	} //end main
 }
+
